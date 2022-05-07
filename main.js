045ac5c3d9b987fc9ea8dc787048e918f2993354
komorebi0722 <seonga1995@naver.com>
2022-05-07 18:56:51 +0900

    JS : Home Tranparent

--- main.js
+++ main.js
@@ -29,7 +29,18 @@ homeContact.addEventListener('click',()=>{
     scrollIntoView('#contact');
 });
 
+// Make home slowly fade to transparent as the window scrolls down
+const home=document.querySelector('.home__container');
+const homeHeight = home.getBoundingClientRect().height;
+document.addEventListener('scroll',()=>{
+    home.style.opacity=1-window.scrollY/homeHeight;
+});
+
+
+
 function scrollIntoView(selector){
     const scrollTO=document.querySelector(selector);
     scrollTO.scrollIntoView({behavior:'smooth'});
 };
+
+ 
