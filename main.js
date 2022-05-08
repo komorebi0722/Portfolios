a7eb1e573bd283a4bb7172edca0013e9f0008b60
komorebi0722 <seonga1995@naver.com>
2022-05-08 16:12:00 +0900

    JS : Navbar menu and project UI

--- main.js
+++ main.js
@@ -12,14 +12,21 @@ document.addEventListener('scroll',()=>{
 });
 
 // Handle scrolloing when tapping on the navbar menu
-const navbarMneu=document.querySelector('.navbar__menu');
-navbarMneu.addEventListener('click',(event)=>{
+const navbarMenu=document.querySelector('.navbar__menu');
+navbarMenu.addEventListener('click',(event)=>{
     const target=event.target;
     const link=target.dataset.link;
     if(link==null){
         return;
     }
+    navbarMenu.classList.remove('open');
     scrollIntoView(link);
+});
+
+// Navbar toggle button for small screen
+const navbarToggleBtn=document.querySelector('.navbar__toggle-btn');
+navbarToggleBtn.addEventListener('click',()=>{
+    navbarMenu.classList.toggle('open');
 });
 
 // Handle click on "contact me" button on home
